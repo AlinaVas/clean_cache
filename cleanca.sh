2606acca9e4c588691cd1172155e10ecddb1e141
Alina Fesyk <fesyk.alina@gmail.com>
2018-12-10 16:06:18 +0200

    limited depth of search via "find" command
    ERROR OCCURED:
    find: -delete: rmdir(/Users/***/Library/Caches.42_cache_bak): No such file or directory
    The reason is find tries to search the directory that was already deleted.
    
    SOLUTION: limit "find" with flags -mindepth 1 and -maxdepth 1, so that it will search
    only through contents of the given directory (1 level of depth).

--- cleanca.sh
+++ cleanca.sh
@@ -1,4 +1,4 @@
-# CLEAN CACHE #
+# SMALL SCRIPT FOR CACHE REMOVAL #
 
 PURPLE='\033[1;95m'
 BLUE='\033[1;94m'
@@ -7,8 +7,8 @@ NOCLR='\033[0m'
 BEFORE=$(df -h | grep "$(whoami)" | awk {'print $4'})
 
 find $HOME/Library/ -mindepth 1 -maxdepth 1 -name "*42_cache*" -delete
-find $HOME -maxdepth 1 -name ".*42_cache*" -or -name ".*zcompdump*" -delete
-find $HOME/.Trash -mindepth 1 -delete
+find $HOME -mindepth 1 -maxdepth 1 -name ".*42_cache*" -or -name ".*zcompdump*" -delete
+find $HOME/.Trash -mindepth 1 -maxdepth 1 -delete
 
 #This will delete TELEGRAM media cache that is older than 7 days
 find $HOME/Library/Group\ Containers/*.Telegram/account-*/postbox/media/ -maxdepth 1 -mtime +7 -delete
