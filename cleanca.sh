1f73529d03e967eb811e16c8fc526056fb882adb
Alina Fesyk <fesyk.alina@gmail.com>
2018-12-10 16:32:11 +0200

    changed the method of removal
    PROBLEM:
    Files and directories with whitespaces in their names were ignored.
    
    SOLUTION:
    xargs -0 /bin/rm -rf  deals with whitespaces, where xargs is list of 
    arguments set with find command.
    -0 flag set ONLY '\0' as separator (works only with -print0 in 'find')

--- cleanca.sh
+++ cleanca.sh
@@ -6,9 +6,9 @@ NOCLR='\033[0m'
 
 BEFORE=$(df -h | grep "$(whoami)" | awk {'print $4'})
 
-find $HOME/Library/ -mindepth 1 -maxdepth 1 -name "*42_cache*" -delete
-find $HOME -mindepth 1 -maxdepth 1 -name ".*42_cache*" -or -name ".*zcompdump*" -delete
-find $HOME/.Trash -mindepth 1 -maxdepth 1 -delete
+find $HOME/Library -mindepth 1 -maxdepth 1 -name "*42_cache*" -print0 | xargs -0 /bin/rm -rf
+find $HOME -mindepth 1 -maxdepth 1 -name ".*42_cache*" -or -name ".*zcompdump*" -print0 | xargs -0 /bin/rm -rf
+find $HOME/.Trash -mindepth 1 -maxdepth 1 -print0 | xargs -0 /bin/rm -rf
 
 #This will delete TELEGRAM media cache that is older than 7 days
 find $HOME/Library/Group\ Containers/*.Telegram/account-*/postbox/media/ -maxdepth 1 -mtime +7 -delete
