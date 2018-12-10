2b3e00ddda82070cfe26b79b22e64f203deece37
Alina Fesyk <fesyk.alina@gmail.com>
2018-12-10 16:42:35 +0200

    splitted search
    PROBLEM:
    -name ".*42_cache*" -or -name ".*zcompdump*" - only files matching 
    the second pattern were deleted.
    
    SOLUTION:
    Split one command into two.

--- cleanca.sh
+++ cleanca.sh
@@ -7,7 +7,8 @@ NOCLR='\033[0m'
 BEFORE=$(df -h | grep "$(whoami)" | awk {'print $4'})
 
 find $HOME/Library -mindepth 1 -maxdepth 1 -name "*42_cache*" -print0 | xargs -0 /bin/rm -rf
-find $HOME -mindepth 1 -maxdepth 1 -name ".*42_cache*" -or -name ".*zcompdump*" -print0 | xargs -0 /bin/rm -rf
+find $HOME -mindepth 1 -maxdepth 1 -name ".*42_cache*" -print0 | xargs -0 /bin/rm -rf
+find $HOME -mindepth 1 -maxdepth 1 -name ".*zcompdump*" -print0 | xargs -0 /bin/rm -rf
 find $HOME/.Trash -mindepth 1 -maxdepth 1 -print0 | xargs -0 /bin/rm -rf
 
 #This will delete TELEGRAM media cache that is older than 3 days
